bcf600219cd3c54d3e9e5ff2fa131773c286ce4e
MaxOzymok <ozymokmax1@ukr.net>
2020-06-06 23:22:47 +0300

    Updates

---
+++ precache-manifest.d1e022e285e2ebf842509041045c228b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b193cb80379f342bea5981f285d9d7",
+    "url": "/demoChat/index.html"
+  },
+  {
+    "revision": "964b815880d5ac281101",
+    "url": "/demoChat/static/css/main.1b91caa1.chunk.css"
+  },
+  {
+    "revision": "ea4c8d19a3794e93bf71",
+    "url": "/demoChat/static/js/2.7da90d40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demoChat/static/js/2.7da90d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964b815880d5ac281101",
+    "url": "/demoChat/static/js/main.43f0410b.chunk.js"
+  },
+  {
+    "revision": "2c3a56f3c007b19175ba",
+    "url": "/demoChat/static/js/runtime-main.4984b180.js"
+  }
+]);
